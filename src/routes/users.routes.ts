21b24d1dcf2586e624dbf6eaeddd747de4d09d20
Lucas <lsengenheirocivil@gmail.com>
2023-02-01 19:54:58 -0300

    feat: create route profile

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import createUserController from "../controllers/users/createUser.controller";
+import userProfileController from "../controllers/users/userProfile.controller";
+import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
 import ensureEmailAlreadyExistMiddleware from "../middlewares/ensureEmailAlreadyExists.middleware";
 
 const userRoutes = Router();
 
 userRoutes.post("", ensureEmailAlreadyExistMiddleware, createUserController);
+userRoutes.get("/profile", ensureAuthMiddleware, userProfileController);
 
 export default userRoutes;
