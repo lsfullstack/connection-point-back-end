b6adc5894ad4316e849c236046f1783b920a4b9e
Lucas <lsengenheirocivil@gmail.com>
2023-02-02 20:58:07 -0300

    feat: create delete contact route

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import createContactController from "../controllers/contacts/createContact.controller";
+import deleteContactController from "../controllers/contacts/deleteContact.controller";
 import listContactsController from "../controllers/contacts/listContacts.controller";
 import retrieveContactController from "../controllers/contacts/retrieveContact.controller";
 import updateContactController from "../controllers/contacts/updateContact.controller";
@@ -11,5 +12,6 @@ contactRoutes.post("/:id", ensureAuthMiddleware, createContactController);
 contactRoutes.get("/:id", ensureAuthMiddleware, listContactsController);
 contactRoutes.get("/client/:id", ensureAuthMiddleware, retrieveContactController);
 contactRoutes.patch("/:id", ensureAuthMiddleware, updateContactController);
+contactRoutes.delete("/:id", ensureAuthMiddleware, deleteContactController);
 
 export default contactRoutes;
