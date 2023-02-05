188fe100d4ffdd4eb6daf75f76837bc5a2f13068
Lucas <lsengenheirocivil@gmail.com>
2023-02-05 11:26:17 -0300

    fix: data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,12 +3,15 @@ import "dotenv/config";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
   synchronize: false,
   logging: true,
-  entities: process.env.NODE_ENV === "production" ? ["dist/src/entities/*.js"] : ["src/entities/*.ts"],
-  migrations: process.env.NODE_ENV === "production" ? ["dist/src/migrations/*.js"] : ["src/migrations/*.ts"],
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/migrations/*.ts"]
 });
 
 export default AppDataSource;
